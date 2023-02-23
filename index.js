54dd2485059429d026fe946bd18dcd8b19166137
Room4Renzo <lorenzo.afw@gmail.com>
2023-02-23 18:31:12 +0000

    Haver stored returned user input in variables, stored the variables in an array and attempted to write a file and appwnd the information, but im receiving a weird terminal error. The question displays with a question mark at the beginning and weird characters at the end. like the esc key on a keyboard with numbers. No idea what they mean but will seek help to understand and fix

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// const fs = require("fs");
-// const path = require('path');
-// const inquirer = require("inquirer");
-// const generateMarkdown = require("./utils/generateMarkdown");
 import fs from "fs";
 import path from "path";
 import inquirer from "inquirer";
-import generateMarkdown from "./utils/generateMarkdown.js";
+
+
 
 
 // array of questions for user
@@ -19,12 +16,59 @@ const questions = [
     { type: "list", name: "license", message: "Please select a license.", choices: ["MIT", "Apache", "GPL", "BSD", "None"] },
     { type: "input", name: "github", message: "Please provide your GitHub username." },
     { type: "input", name: "email", message: "Please provide your email address." },
-
-
 ];
+
+inquirer
+    .prompt(questions)
+    .then((answers) => {
+        answers = [title, description, installation, usage, contribution, test, license, github, email];
+    });
+
+    const title = process.argv[2];
+    const description = process.argv[3];
+    const installation = process.argv[4];
+    const usage = process.argv[5];
+    const contribution = process.argv[6];
+    const test = process.argv[7];
+    const license = process.argv[8];
+    const github = process.argv[9];
+    const email = process.argv[10];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+    fileName = "README.md";
+    data =
+        `# ${title}
+    ## Description
+    ${description}
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    ## Installation
+    ${installation}
+    ## Usage
+    ${usage}
+    ## License
+    ${license}
+    ## Contributing
+    ${contribution}
+    ## Tests
+    ${test}
+    ## Questions
+    If you have any questions, please contact me at ${email}. You can find more of my work at [${github}]`;
+
+
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+
+
 
 };
 
